ae20648fe5dbf7f3d78e12f148ed08cb653814ad
川岡優太 <kawaokayuta@ky.local>
2021-04-20 23:48:34 +0900

    2

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.conponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>一緒にReactを学びましょう</p>
+      </div>
+    )
+  }
+}
+
+export default App;
